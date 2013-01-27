b17ac978e8a3743148fcfe60b371d8be181a8b2c
Edward Kmett <ekmett@gmail.com>
2013-01-26 22:34:23 -0500

    inlined bitraversable code

--- src/Data/Semigroup/Bitraversable.hs
+++ src/Data/Semigroup/Bitraversable.hs
@@ -25,19 +25,25 @@ import Data.Tagged
 class (Bifoldable1 t, Bitraversable t) => Bitraversable1 t where
   bitraverse1 :: Apply f => (a -> f b) -> (c -> f d) -> t a c -> f (t b d)
   bitraverse1 f g  = bisequence1 . bimap f g
+  {-# INLINE bitraverse1 #-}
 
   bisequence1 :: Apply f => t (f a) (f b) -> f (t a b)
   bisequence1 = bitraverse1 id id
+  {-# INLINE bisequence1 #-}
 
 bifoldMap1Default :: (Bitraversable1 t, Semigroup m) => (a -> m) -> (b -> m) -> t a b -> m
 bifoldMap1Default f g = getConst . bitraverse1 (Const . f) (Const . g)
+{-# INLINE bifoldMap1Default #-}
 
 instance Bitraversable1 Either where
   bitraverse1 f _ (Left a) = Left <$> f a
   bitraverse1 _ g (Right b) = Right <$> g b
+  {-# INLINE bitraverse1 #-}
 
 instance Bitraversable1 (,) where
   bitraverse1 f g (a, b) = (,) <$> f a <.> g b
+  {-# INLINE bitraverse1 #-}
 
 instance Bitraversable1 Tagged where
   bitraverse1 _ g (Tagged b) = Tagged <$> g b
+  {-# INLINE bitraverse1 #-}
