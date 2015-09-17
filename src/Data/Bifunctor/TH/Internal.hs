642570e3b60ed4e86ad2af3ea3f0dda5c533ca44
RyanGlScott <ryan.gl.scott@gmail.com>
2015-09-17 14:11:21 -0400

    Use findWithDefault instead of lookup
    As pointed out to me by @DNNX

--- src/Data/Bifunctor/TH/Internal.hs
+++ src/Data/Bifunctor/TH/Internal.hs
@@ -13,9 +13,8 @@ module Data.Bifunctor.TH.Internal where
 
 import           Data.Function (on)
 import           Data.List
-import qualified Data.Map as Map (fromList, lookup)
+import qualified Data.Map as Map (fromList, findWithDefault)
 import           Data.Map (Map)
-import           Data.Maybe
 import qualified Data.Set as Set
 import           Data.Set (Set)
 
@@ -69,7 +68,7 @@ mkSubst vs ts =
 
 subst :: Subst -> Type -> Type
 subst subs (ForallT v c t) = ForallT v c $ subst subs t
-subst subs t@(VarT n)      = fromMaybe t $ Map.lookup n subs
+subst subs t@(VarT n)      = Map.findWithDefault t n subs
 subst subs (AppT t1 t2)    = AppT (subst subs t1) (subst subs t2)
 subst subs (SigT t k)      = SigT (subst subs t) k
 subst _ t                  = t
