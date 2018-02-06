e07c7fa4f1b396ac6221d8978868bf09e996095a
Ryan Scott <ryan.gl.scott@gmail.com>
2018-02-06 11:08:42 -0500

    Don't enable Safe on GHC 7.2
    GHC 7.2's treatment of Safe Haskell is incredibly buggy, and even
    more so with `cabal new-build`, resulting in
    https://github.com/ekmett/semigroupoids/issues/69.

--- src/Data/Bifunctor/Functor.hs
+++ src/Data/Bifunctor/Functor.hs
@@ -2,8 +2,10 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeOperators #-}
 
-#if __GLASGOW_HASKELL__ >= 702
+#if __GLASGOW_HASKELL__ >= 704
 {-# LANGUAGE Safe #-}
+#elif __GLASGOW_HASKELL__ >= 702
+{-# LANGUAGE Trustworthy #-}
 #endif
 
 #if __GLASGOW_HASKELL__ >= 706
