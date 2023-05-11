57299e225915415165e19d0f40357962b60466d2
Ryan Scott <rscott@galois.com>
2023-05-11 10:39:59 -0400

    Adapt to (~) becoming a type operator, pt. 2
    I missed a spot in
    e9cfde28aa31086c3735f32dab1027a77de0bbf2.

--- src/Data/Bifunctor/Biff.hs
+++ src/Data/Bifunctor/Biff.hs
@@ -9,6 +9,7 @@
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE Trustworthy #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 
 -- |
 -- Copyright   :  (C) 2008-2021 Edward Kmett
