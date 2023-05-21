5ac89006cbebdd729ab0a7ffd35ec3d4ab5000e2
Ellie Hermaszewska <git@monoid.al>
2023-05-21 15:03:08 +0800

    Fix syntax introducing Haddock comment

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS_GHC -F -pgmF hspec-discover #-}
 
---|
+-- |
 -- Module:      Spec
 -- Copyright:   (C) 2008-2023 Edward Kmett, (C) 2015 Ryan Scott
 -- License:     BSD-2-Clause OR Apache-2.0
